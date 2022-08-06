155e66f8a6e152ef3471264a6feccec62005abeb
Arthur Maydanuk <arthur.maydanuk@akveo.com>
2022-08-06 18:58:17 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Exercise.log API
+
+## Description
+
+The API for Exercise.log web application.
+
+## Installation
+
+```bash
+$ npm install
+```
+
+## Running the app
+
+```bash
+# development
+$ npm run start
+
+# watch mode
+$ npm run start:dev
+
+# production mode
+$ npm run start:prod
+```
---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const PORT = process.env.PORT || 3000;
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Exercise.log')
+    .setDescription('The Exercise.log API documentation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(PORT, () => console.log(`Server started on ${PORT} port`));
+}
+
+bootstrap();
