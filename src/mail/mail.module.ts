0a9b27ae7356e1c2b2be1c6e5780ae6ffe56f2b0
--global <--global>
2022-08-14 17:21:32 +0300

    fix: smpt connection

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -4,21 +4,13 @@ import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { MailService } from './mail.service';
 import { join } from 'path';
+import * as sendInBlue from 'nodemailer-sendinblue-transport';
 
 @Module({
   imports: [
     MailerModule.forRootAsync({
       useFactory: (config: ConfigService) => ({
-        transport: {
-          host: config.get<string>('MAIL_HOST'),
-          port: config.get<number>('MAIL_PORT'),
-          secure: true,
-          auth: {
-            type: 'OAuth2',
-            user: config.get<string>('MAIL_FROM'),
-            accessToken: config.get<string>('MAIL_TOKEN'),
-          },
-        },
+        transport: sendInBlue({ apiKey: config.get<string>('MAIL_TOKEN') }),
         defaults: {
           from: `"Exercise.log" ${config.get<string>('MAIL_FROM')}`,
         },
